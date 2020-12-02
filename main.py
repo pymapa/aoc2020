a3ce394ff860fc6b60fbbcd2e3b2ab74f6bfccbf
Pyry Pajunpää <pyry@identio.fi>
2020-12-02 13:13:46 +0200

    day2a

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import re
 data = open("data.txt")
 lines = data.read().split("\n")
 
-def checkDoubleEntry(x, y):
-  for z in lines:
-    if(int(x) + int(y) + int(z) == 2020):
-      print(int(x) * int(y) * int(z))
+def main():
+  validCount = 0
 
+  def isValidEntry(x):
+    passwordLineParts = re.split('-| |:', x)
+    min = int(passwordLineParts[0])
+    max = int(passwordLineParts[1])
+    requiredChar = passwordLineParts[2]
+    password = passwordLineParts[4]
+    occurrence = password.count(requiredChar)
+    if min <= occurrence <= max:
+      return True
+    return False
 
-def checkEntry(x):
-  for y in lines:
-    checkDoubleEntry(x, y)
+  for x in lines:
+    if isValidEntry(x):
+      validCount += 1
 
-for x in lines:
-  checkEntry(x)
+  print(validCount)
 
-  
+main()
