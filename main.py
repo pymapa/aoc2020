093d16393e8082d9ab096dceb7a8f97f6b74e43b
Pyry Pajunpää <pyry@identio.fi>
2020-12-03 13:07:01 +0200

    day2b

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ lines = data.read().split("\n")
 def main():
   validCount = 0
 
+  # def isValidEntry(x):
+  #   passwordLineParts = re.split('-| |:', x)
+  #   min = int(passwordLineParts[0])
+  #   max = int(passwordLineParts[1])
+  #   requiredChar = passwordLineParts[2]
+  #   password = passwordLineParts[4]
+  #   occurrence = password.count(requiredChar)
+  #   return min <= occurrence <= max:
+
   def isValidEntry(x):
     passwordLineParts = re.split('-| |:', x)
     min = int(passwordLineParts[0])
     max = int(passwordLineParts[1])
     requiredChar = passwordLineParts[2]
     password = passwordLineParts[4]
-    occurrence = password.count(requiredChar)
-    if min <= occurrence <= max:
-      return True
-    return False
+    return bool(password[min - 1] == requiredChar) != bool(password[max - 1] == requiredChar)
 
   for x in lines:
     if isValidEntry(x):
