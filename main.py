bc12bb9dc1d8453b256adbb9ede0b440f142e961
Pyry Pajunpää <pyry@identio.fi>
2020-12-04 15:40:23 +0200

    day4a

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
+import re
 data = open("data.txt")
-array = data.read().split("\n")
 
-class Step:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+def getPassports():
+    return data.read().split("\n\n")
 
-  def trees(self):
-    return travelWithStep(self.x, self.y)
 
-def isTree(line, x):
-  return line[x] == '#'
+def getPassportEntries(passportStr):
+    return re.split(" |\n", passportStr)
 
-def travelWithStep(stepX, stepY):
-  positionX = 0
-  treeCount = 0
-  for line in array[::stepY]:
-    length = len(line)
-    if isTree(line, positionX):
-      treeCount += 1
-    if positionX + stepX <= length - 1:
-      positionX += stepX
-    else:
-      positionX = positionX + stepX - length
-  return treeCount
+
+def isValidPassport(entries):
+    foundEntries = 0
+    requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    for entry in entries:
+        if entry.split(":")[0] in requiredFields:
+            foundEntries += 1
+    return foundEntries == 7
+
 
 def main():
-  steps = []
-  steps.append(Step(1, 1))
-  steps.append(Step(3, 1))
-  steps.append(Step(5, 1))
-  steps.append(Step(7, 1))
-  steps.append(Step(1, 2))
+    validPassports = 0
+    passports = getPassports()
 
-  treesMultiplied = 1
-  for step in steps:
-    treesMultiplied = treesMultiplied * step.trees()
+    for passportStr in passports:
+        if isValidPassport(getPassportEntries(passportStr)):
+            validPassports += 1
+    print(validPassports)
 
-  print(treesMultiplied)
 main()
