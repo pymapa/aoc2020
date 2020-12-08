92bf705e536392babffecfd3eee6aedf5a23c535
Pyry Pajunpää <pyry@identio.fi>
2020-12-08 20:23:06 +0200

    day7ab

--- planeticket/__init__.py
+++ planeticket/__init__.py
@@ -1,20 +1,77 @@
 import re
 file = open("data.txt")
 
-def getPositiveAnswers(group):
-  rows = group.split("\n")
-  answers = rows[0]
-  for row in rows:
-    answers = [value for value in row if value in answers]
-  return sorted(set(answers))
+class Bag:
+  def __init__(self, name='root', bags=None):
+    self.name = name.strip()
+    self.bags = []
+    if bags is not None:
+      for bag in bags:
+        self.add_child(bag)
+  
+  def __hash__(self):
+    return hash(self.name)
+
+  def add_child(self, node):
+    self.bags.append(node)
+
+  def hasChild(self, bagName):
+    if len(self.bags) == 0:
+      return False
+    res = [item for item in self.bags
+      if item[1] == bagName]
+    return len(res) > 0
+
+  def __repr__(self):
+    return '{}, {}'.format(self.name, self.bags)
+
+  def __eq__(self, other):
+    return self.name == other.name and \
+      self.bags == other.bags
+
+def createBag(rule):
+  parts = re.split(' bags |contain | bags| bag| bags|, ', rule)
+  bag = Bag(parts.pop(0), [])
+  if 'no other bags' in rule:
+    return bag
+  for part in parts:
+    if part != '' and part != '.':
+      number = int(re.findall(r'\d+', part)[0])
+      text = ''.join(i for i in part if not i.isdigit())
+      bag.add_child((number, text.strip()))
+  return bag
+
+def findParentBags(bags, name, acc):
+  acc += 1
+  parentBags = []
+  for bag in bags:
+    if bag.hasChild(name):
+      # print('bag {} contains bag {}'.format(bag, name) )
+      parentBags.append(bag)
+      parentBags.extend(findParentBags(bags, bag.name, acc))
+  return set(parentBags)
+
+def countChildBags(bags, name, factor):
+  bag = [x for x in bags if x.name == name][0]
+  count = 0
+  for child in bag.bags:
+    amount = child[0]
+    bagName = child[1]
+    count += amount * factor
+    count += countChildBags(bags, bagName, amount * factor)
+  return count
 
 def main():
-  groups = file.read().split("\n\n")
-  sum = 0
-  for group in groups:
-    sum += len(getPositiveAnswers(group))
+  rules = file.read().split("\n")
+  bags = []
+  for rule in rules:
+    bags.append(createBag(rule))
+  
+  parentBags = findParentBags(bags, 'shiny gold', 0)
+  print('Parent bags: ', len(parentBags))
 
-  print(sum)
+  childBagsCount = countChildBags(bags, 'shiny gold', 1)
+  print('Child bags: ', childBagsCount)
 
 if __name__ == "__main__":
   main()
--- test.py
+++ test.py
@@ -1,15 +1,71 @@
 import unittest
 
-from planeticket import getPositiveAnswers
+from planeticket import createBag, findParentBags, countChildBags, Bag
 class PlaneticketTest(unittest.TestCase):
 
-  def test_getPositiveAnswers(self):
+  def test_createBag(self):
     """
-    Test counting positive answers
+    Test creating bag
     """
-    group = 'abc\naabcd\nfgbc'
-    self.assertEqual(getPositiveAnswers(group), ['b', 'c'])
+    rule = 'faded plum bags contain 2 wavy cyan bags, 1 navy turquoise bag.'
+    bag = Bag('faded plum', [(2, 'wavy cyan'), (1, 'navy turquoise')])
+    self.assertEqual(createBag(rule), bag)
 
+  def test_hasChild(self):
+    """
+    Test finding by child
+    """
+    bag = Bag('faded plum', [(2, 'wavy cyan'), (1, 'navy turquoise')])
+    res = bag.hasChild('wavy cyan')
+    self.assertTrue(res)
+    bag1 = Bag('gaded plum', [(2, 'wavy cyan'), (1, 'navy turquoise')])
+    res1 = bag1.hasChild('gaded plum')
+    self.assertFalse(res1)
+
+  def test__findParentBags(self):
+    """
+    Test finding parent bags
+    """
+    file = """light red bags contain 1 bright white bag, 2 muted yellow bags.
+      dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+      bright white bags contain 1 shiny gold bag.
+      muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+      shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+      dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+      vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+      faded blue bags contain no other bags.
+      dotted black bags contain no other bags."""
+    
+
+    rules = file.split("\n")
+    bags = []
+    for rule in rules:
+      bag = createBag(rule)
+      bags.append(bag)
+    parentBags = findParentBags(bags, 'shiny gold', 0)
+    self.assertEqual(len(parentBags), 4)
+
+  def test__findChildBags(self):
+    """
+    Test finding child bags
+    """
+    file = """
+    shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags."""
+
+    rules = file.split("\n")
+    bags = []
+    for rule in rules:
+      bag = createBag(rule)
+      bags.append(bag)
+
+    childBagsCount = countChildBags(bags, 'shiny gold', 1)
+    self.assertEqual(childBagsCount, 126)
 
 if __name__ == "__main__":
   unittest.main()
